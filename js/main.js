9161958212e369ed9aea907eba203fb46c70c0de
Darya Iakovleva <888darya@gmail.com>
2022-03-09 23:38:42 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandInt = (min, max) => {
-  if (min < 0 || max < 0) return 'Рип. Оба числа должны быть больше нуля';
+  if (min < 0 || max < 0) {
+    return 'Рип. Оба числа должны быть больше нуля';
+  }
   return Math.floor(min + Math.random() * (max - min + 1));
 };
 
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна.
 const getStrLen = (str, maxLen) => str.length <= maxLen;
+console.log(getRandInt(3, 4));
+console.log('hfjdkjs skdhhfsk', 3);
