537637b588a10e8eac9af61391931e8d08a39c4c
Keks <keks@htmlacademy.ru>
2022-03-20 18:09:18 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/pictures.js
@@ -0,0 +1,29 @@
+import {
+  createPosts
+} from './posts.js';
+
+const pictures = document.querySelector('.pictures');
+const template = document
+  .querySelector('#picture')
+  .content.querySelector('.picture');
+const postsList = createPosts();
+
+const createPicture = (postInfo) => {
+  const el = template.cloneNode(true);
+  el.id = postInfo.id;
+  el.querySelector('.picture__img').src = postInfo.url;
+  el.querySelector('.picture__likes').textContent = postInfo.likes;
+  el.querySelector('.picture__comments').textContent = postInfo.comments.length;
+
+  return el;
+};
+
+const renderPicture = (picture, container) => {
+  const fragment = document.createDocumentFragment();
+  fragment.append(picture);
+  container.append(fragment);
+};
+
+for (const i in postsList) {
+  renderPicture(createPicture(postsList[i]), pictures);
+}
